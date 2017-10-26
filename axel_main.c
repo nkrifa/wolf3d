e3a39885f785d8c4ee0722ac75003f2effc96e24
Axel FIODIERE <afiodier@e3r8p4.42.fr>
2017-10-26 23:20:58 +0200

    sa marche pas

--- axel_main.c
+++ axel_main.c
@@ -2,6 +2,7 @@
 # include "./minilibx_macos/mlx.h"
 # include <math.h>
 # include <stdlib.h>
+# include <stdio.h>
 # define WIN_W 880
 # define WIN_H 880
 
@@ -39,6 +40,7 @@ char world[25] = "1111110001100011000111111";
 
 void	point_to_point_image(int x, int start, int end, int color, int *scene)
 {
+//printf(" %d , %d\n", start, end);
 	while (start <= end)
 	{
 		scene[x + (start * WIN_W)] = color;
@@ -67,17 +69,17 @@ t_env	*init_env(char *s)
 
 int main(int argc, char **argv)
 {
-	static double	pos[2] = {2,1};
+	static double	pos[2] = {2.0,2.0};
 	double			camX;
-	static double	dir[2] = {0,0};
+	static double	dir[2] = {0.0,-1.0};
 	double			raydir[2];
-	static double	plane[2] = {0,0.66};
+	static double	plane[2] = {0.0,0.66};
 	double			raypos[2];
 	int				x;
 	int				map[2];
-	char			step[2];
-	char			hit;
-	char			side;
+	int				step[2];
+	int				hit;
+	int				side;
 	double			sidedist[2];
 	double			deltadist[2];
 	double			perpwalldist;
@@ -89,28 +91,28 @@ int main(int argc, char **argv)
 		//mlx_hook(e->win, 17, (1L << 17), close_prog, e);
 		//mlx_hook(e->win, 2, (1L << 0), key_press, e);
 	}
-	x = 0;
+	x = -1;
 	while(++x < WIN_W)
 	{
-		camX = 2 * x / (double)WIN_W - 1;
-		*raypos = *pos;
+		camX = (2 * x / (double)WIN_W) - 1;
+		raypos[0] = pos[0];
 		raypos[1] = pos[1];
-		*raydir = *dir + *plane + camX;
+		raydir[0] = dir[0] + plane[0] + camX;
 		raydir[1] = dir[1] + plane[1] + camX;
-		*map = (int)*raypos;
+		map[0] = (int)raypos[0];
 		map[1] = (int)raypos[1];
-		*deltadist = sqrtf(1 + (raydir[1] * raydir[1]) / (*raydir * *raydir));
-		deltadist[1] = sqrtf(1 + (*raydir * *raydir) / (raydir[1] * raydir[1]));
-		hit = 0;
-		if (*raydir < 0)
+		deltadist[0] = sqrtf(1.0 + (raydir[1] * raydir[1]) / (raydir[0] * raydir[0]));
+		deltadist[1] = sqrtf(1.0 + (raydir[0] * raydir[0]) / (raydir[1] * raydir[1]));
+		hit = 0;		
+		if (raydir[0] < 0)
 		{
-			*step = -1;
-			*sidedist = (*raypos - *map) * *deltadist;
+			step[0] = -1;
+			sidedist[0] = (raypos[0] - map[0]) * deltadist[0];
 		}
 		else
 		{
-			*step = 1;
-			*sidedist = (*map + 1.0 - *raypos) * *deltadist;
+			step[0] = 1;
+			sidedist[0] = (map[0] + 1.0 - raypos[0]) * deltadist[0];
 		}
 		if (raydir[1] < 0)
 		{
@@ -124,30 +126,36 @@ int main(int argc, char **argv)
 		}
 		while (hit == 0) /*wall hit ? */
 		{
-			if(*sidedist < sidedist[1])
+			if(sidedist[0] < sidedist[1])
 			{
-				*sidedist += *deltadist;
-				*map += (int)*step;
+				sidedist[0] += deltadist[0];
+				map[0] += step[0];
 				side = 0;
 			}
 			else
 			{
 				sidedist[1] += deltadist[1];
-				map[1] += (int)step[1];
+				map[1] += step[1];
 				side = 1;
 			}
-			if (world[*map + (map[1] * 5)] > 0)
+			printf("%c\n", world[map[0] + (map[1] * 5)]);
+			if (world[map[0] + (map[1] * 5)] != '0')
 				hit = 1;
 		}
 		if (side)
-			perpwalldist = (map[1] - raypos[1] + (1 - step[1]) / 2) / raydir[1];
+		{
+			perpwalldist = ((double)map[1] - raypos[1] + (1 - (double)step[1]) / 2) / raydir[1];
+		}
 		else
-			perpwalldist = (*map - *raypos + (1 - *step) / 2) / *raydir;
-		lineheight = (int)(WIN_H/perpwalldist);
-		point_to_point_image(x, (-lineheight/2 + WIN_H/2), (lineheight/2 + WIN_H/2), ROUGE, e->f->data);
+		{
+			perpwalldist = ((double)map[0] - raypos[0] + (1 - (double)step[0]) / 2) / raydir[0];
+		}
+		lineheight = (int)((double)WIN_H/perpwalldist);
+		
+		point_to_point_image(x, (int)(WIN_H/2 - lineheight/2) < 0 ? 0 : (int)(WIN_H/2 - lineheight/2), (int)(lineheight/2 + WIN_H/2), ROUGE, e->f->data);
 		
 	}
-	mlx_put_image_to_window(e->mlx, e->win, e->f->img, 0, 0);
+	mlx_put_image_to_window(e->mlx, e->win, e->f->img_ptr, 0, 0);
 	mlx_loop(e->mlx);
 	mlx_destroy_window(e->mlx, e->win);
 	return(0);
