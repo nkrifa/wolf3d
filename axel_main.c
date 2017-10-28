d490f0bf74624e24e36240084f2a156511ea40e2
Nassim KRIFA <nkrifa@e1r3p1.42.fr>
2017-10-28 22:24:15 +0200

    tttt

--- axel_main.c
+++ axel_main.c
@@ -35,6 +35,7 @@ typedef struct s_cam
 	int		stepy;
 	double	deltadistx;
 	double	deltadisty;
+	int		hit;
 }	t_cam;
 
 typedef struct s_player
@@ -83,20 +84,20 @@ typedef struct	s_env
 int worldMap[24][24]=
 {
   {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+  {1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,2,2,2,2,0,0,0,0,0,0,0,0,1},
   {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
   {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
   {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
   {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,1,2,1,1,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1},
   {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
   {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
   {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
@@ -141,6 +142,7 @@ t_env	*init_env(char *s)
 	e->p.diry = 0;
 	e->p.posx = 12.0;
 	e->p.posy = 12.0;
+	e->cam.hit = 0;
 	e->cam.planex = 0.0;
 	e->cam.planey = 0.6;
 	e->mlx = mlx_init();
@@ -164,7 +166,6 @@ void	ft_raycast(t_env *e)
 //	static double	plane[2] = {0.0,0.66};
 //	double			raypos[2];
 //	int				x;
-	t_cam		cam;
 //	int				map[2];
 //	int				step[2];
 	int				hit;
@@ -175,71 +176,71 @@ void	ft_raycast(t_env *e)
 	int				lineheight;
 	
 	ft_bzero(e->f->data, 4 * WIN_H * WIN_W);
-	cam.screen_index = -1;
-	while(++cam.screen_index < WIN_W)
+	e->cam.screen_index = -1;
+	while(++e->cam.screen_index < WIN_W)
 	{
-		camX = (2 * cam.screen_index / (double)WIN_W) - 1;
-		cam.rayposx = e->p.posx;
-		cam.rayposy = e->p.posy;
-		cam.raydirx = e->p.dirx + e->cam.planex + camX;
-		cam.raydiry = e->p.diry + e->cam.planey + camX;
-		e->mapx = (int)cam.rayposx;
-		e->mapy = (int)cam.rayposy;
-		e->cam.deltadistx = sqrtf(1.0 + (cam.raydiry * cam.raydiry) / (cam.raydirx * cam.raydirx));
-		e->cam.deltadisty = sqrtf(1.0 + (cam.raydirx * cam.raydirx) / (cam.raydiry * cam.raydiry));
+		camX = (2 * e->cam.screen_index / (double)WIN_W) - 1;
+		e->cam.rayposx = e->p.posx;
+		e->cam.rayposy = e->p.posy;
+		e->cam.raydirx = e->p.dirx + e->cam.planex * camX;
+		e->cam.raydiry = e->p.diry + e->cam.planey * camX;
+		e->mapx = (int)e->cam.rayposx;
+		e->mapy = (int)e->cam.rayposy;
+		e->cam.deltadistx = sqrtf(1.0 + (e->cam.raydiry * e->cam.raydiry) / (e->cam.raydirx * e->cam.raydirx));
+		e->cam.deltadisty = sqrtf(1.0 + (e->cam.raydirx * e->cam.raydirx) / (e->cam.raydiry * e->cam.raydiry));
 		hit = 0;		
-		if (cam.raydirx < 0)
+		if (e->cam.raydirx < 0)
 		{
 			e->cam.stepx = -1;
-			cam.sidedistx = (cam.rayposx - (double)e->mapx) * e->cam.deltadistx;
+			e->cam.sidedistx = (e->cam.rayposx - (double)e->mapx) * e->cam.deltadistx;
 		}
 		else
 		{
 			e->cam.stepx = 1;
-			cam.sidedistx = ((double)e->mapx + 1.0 - cam.rayposx) * e->cam.deltadistx;
+			e->cam.sidedistx = ((double)e->mapx + 1.0 - e->cam.rayposx) * e->cam.deltadistx;
 		}
-		if (cam.raydiry < 0)
+		if (e->cam.raydiry < 0)
 		{
 			e->cam.stepy = -1;
-			cam.sidedisty = (cam.rayposy - (double)e->mapy) * e->cam.deltadisty;
+			e->cam.sidedisty = (e->cam.rayposy - (double)e->mapy) * e->cam.deltadisty;
 		}
 		else
 		{
 			e->cam.stepy = 1;
-			cam.sidedisty = ((double)e->mapy + 1.0 - cam.rayposy) * e->cam.deltadisty;
+			e->cam.sidedisty = ((double)e->mapy + 1.0 - e->cam.rayposy) * e->cam.deltadisty;
 		}
 		while (hit == 0) /*wall hit ? */
 		{
 			//printf("%d, %d\n",e->mapx,e->mapy);
-			if(cam.sidedistx < cam.sidedisty)
+			if(e->cam.sidedistx < e->cam.sidedisty)
 			{
-				cam.sidedistx += e->cam.deltadistx;
+				e->cam.sidedistx += e->cam.deltadistx;
 				e->mapx += e->cam.stepx;
-				cam.side = 0;
+				e->cam.side = 0;
 			}
 			else
 			{
-				cam.sidedisty += e->cam.deltadisty;
+				e->cam.sidedisty += e->cam.deltadisty;
 				e->mapy += e->cam.stepy;
-				cam.side = 1;
+				e->cam.side = 1;
 			}
 			//printf("%c\n", world[e->mapx + (e->mapy * 5)]);
-			if (worldMap[e->mapx][e->mapy] > 0)//[e->mapx + (e->mapy * 10)] == '1')
+			if (worldMap[e->mapx][e->mapy] != 0)//[e->mapx + (e->mapy * 10)] == '1')
 			{
 				hit = 1;
 			}
 		}
-		if (cam.side == 0)
+		if (e->cam.side == 0)
 		{
-			perpwalldist = fabs(((double)e->mapx - cam.rayposx + (1 - e->cam.stepx) / 2) / cam.raydirx);
+			perpwalldist = fabs(((double)e->mapx - e->cam.rayposx + (1 - e->cam.stepx) / 2) / e->cam.raydirx);
 		}
 		else
 		{
-			perpwalldist = fabs(((double)e->mapy - cam.rayposy + (1 - e->cam.stepy) / 2) / cam.raydiry);
+			perpwalldist = fabs(((double)e->mapy - e->cam.rayposy + (1 - e->cam.stepy) / 2) / e->cam.raydiry);
 		}
 		lineheight = (int)((double)WIN_HH/perpwalldist);
 		
-		point_to_point_image(cam.screen_index, (int)(WIN_H/2 - lineheight/2) < 0 ? 0 : (int)(WIN_H/2 - lineheight/2), (int)(lineheight/2 + WIN_H/2) >= WIN_H ? WIN_H - 1 : (int)(lineheight/2 + WIN_H/2), cam.side ? BLEU : ROUGE, e->f->data);
+		point_to_point_image(e->cam.screen_index, (int)(WIN_H/2 - lineheight/2) < 0 ? 0 : (int)(WIN_H/2 - lineheight/2), (int)(lineheight/2 + WIN_H/2) >= WIN_H ? WIN_H - 1 : (int)(lineheight/2 + WIN_H/2),e->cam.side ? (e->cam.stepy >= 0 ? ROUGE : BLEU) : (e->cam.stepx >= 0 ? JAUNE : MARRON), e->f->data);
 		
 	}
 	mlx_put_image_to_window(e->mlx, e->win, e->f->img_ptr, 0, 0);
@@ -267,7 +268,7 @@ int		key_hook(int keycode, t_env *e)
 		double oldPlaneX = e->p.planex;
 		e->p.planex = e->p.planex * cos(-0.01) - e->p.planey * sin(-0.01);
 		e->p.planey = oldPlaneX * sin(-0.01) + e->p.planey * cos(-0.01);*/
-		e->p.dirx= e->p.dirx * cos(-0.05) - e->p.diry * sin(0.05);
+		e->p.dirx= e->p.dirx * cos(-0.05) - e->p.diry * sin(-0.05);
 		temp = e->p.dirx;
 		tmp = e->cam.planex;
 		e->p.diry= temp * sin(-0.05) + e->p.diry * cos(-0.05);
